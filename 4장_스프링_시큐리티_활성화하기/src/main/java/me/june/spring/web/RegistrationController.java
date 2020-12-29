75ea1f03e3846c8fb5ffd6140fd05b33b37ff274
ncucu <ncucu.me@kakaocommerce.com>
2020-12-29 17:18:19 +0900

    4장 스프링 시큐리티 활성화하기

---
+++ 4장_스프링_시큐리티_활성화하기/src/main/java/me/june/spring/web/RegistrationController.java
@@ -0,0 +1,32 @@
+package me.june.spring.web;
+
+import me.june.spring.repository.jpa.UserJpaRepository;
+import me.june.spring.security.RegistrationForm;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+    private UserJpaRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserJpaRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepository.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
