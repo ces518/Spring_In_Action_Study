e6cae5562163071eabfe422e69db7b4f1890f6a2
ncucu <ncucu.me@kakaocommerce.com>
2020-12-30 23:53:31 +0900

    5장 구성 속성 사용하기

---
+++ 5장_구성_속성_사용하기/src/main/java/me/june/spring/web/OrderController.java
@@ -0,0 +1,82 @@
+package me.june.spring.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.june.spring.domain.Order;
+import me.june.spring.domain.User;
+import me.june.spring.repository.OrderRepository;
+import me.june.spring.repository.jpa.UserJpaRepository;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "taco.orders")
+public class OrderController {
+
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    private final OrderRepository orderRepository;
+    private final UserJpaRepository userRepository;
+
+
+    @GetMapping("/current")
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute Order order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(
+            @Valid Order order,
+            Errors errors,
+            SessionStatus sessionStatus,
+            @AuthenticationPrincipal User user
+    ) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        order.setUser(user);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, 20);
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+    }
+}
