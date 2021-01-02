faf532fd7e22ee110c35335023a89797262b38eb
ncucu <ncucu.me@kakaocommerce.com>
2021-01-02 14:07:21 +0900

    5장 구성 속성 사용하기

--- 5장_구성_속성_사용하기/src/main/java/me/june/spring/web/DesignTacoController.java
+++ 5장_구성_속성_사용하기/src/main/java/me/june/spring/web/DesignTacoController.java
@@ -11,6 +11,7 @@ import me.june.spring.repository.jpa.UserJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
